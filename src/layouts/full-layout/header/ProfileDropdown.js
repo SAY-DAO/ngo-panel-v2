0e9a3b9be02f76efb824b36d5cd80a25c14db141
Ehsan G <ehsan@say.company>
2022-02-28 21:47:13 -0500

    clean up

--- src/layouts/full-layout/header/ProfileDropdown.js
+++ src/layouts/full-layout/header/ProfileDropdown.js
@@ -95,100 +95,10 @@ const ProfileDropdown = () => (
                 lineHeight: '1.235',
               }}
             >
-              My Profile
+              Edit Profile
             </Typography>
             <Typography color="textSecondary" variant="h6" fontWeight="400">
               Account Settings
-            </Typography>
-          </Box>
-        </Box>
-      </MenuItem>
-      <Divider
-        style={{
-          marginTop: 0,
-          marginBottom: 0,
-        }}
-      />
-      <MenuItem
-        sx={{
-          pt: 3,
-          pb: 3,
-        }}
-      >
-        <Box display="flex" alignItems="center">
-          <Button
-            sx={{
-              backgroundColor: (theme) => theme.palette.success.light,
-              color: (theme) => theme.palette.success.main,
-              boxShadow: 'none',
-              minWidth: '50px',
-              width: '45px',
-              height: '40px',
-              borderRadius: '10px',
-            }}
-          >
-            <FeatherIcon icon="shield" width="18" height="18" />
-          </Button>
-          <Box
-            sx={{
-              ml: 2,
-            }}
-          >
-            <Typography
-              variant="h5"
-              sx={{
-                lineHeight: '1.235',
-              }}
-            >
-              My Inbox
-            </Typography>
-            <Typography color="textSecondary" variant="h6" fontWeight="400">
-              Messages & Emails
-            </Typography>
-          </Box>
-        </Box>
-      </MenuItem>
-      <Divider
-        style={{
-          marginTop: 0,
-          marginBottom: 0,
-        }}
-      />
-      <MenuItem
-        sx={{
-          pt: 3,
-          pb: 3,
-        }}
-      >
-        <Box display="flex" alignItems="center">
-          <Button
-            sx={{
-              backgroundColor: (theme) => theme.palette.error.light,
-              color: (theme) => theme.palette.error.main,
-              boxShadow: 'none',
-              minWidth: '50px',
-              width: '45px',
-              height: '40px',
-              borderRadius: '10px',
-            }}
-          >
-            <FeatherIcon icon="credit-card" width="18" height="18" />
-          </Button>
-          <Box
-            sx={{
-              ml: 2,
-            }}
-          >
-            <Typography
-              variant="h5"
-              sx={{
-                lineHeight: '1.235',
-              }}
-            >
-              My Tasks
-            </Typography>
-            <Typography color="textSecondary" variant="h6" fontWeight="400">
-              To-do and Daily Tasks
             </Typography>
           </Box>
         </Box>
