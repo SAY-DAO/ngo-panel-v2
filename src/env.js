3dbd03e581407cdeddc5aeedd5390eeb2c59ef5e
ubuntu-user <ehsan@say.company>
2022-10-27 18:50:26 -0400

    nest server connection

--- src/env.js
+++ src/env.js
@@ -11,17 +11,21 @@ if (env === 'production') {
 } else if (env === 'staging') {
   envApiUrl = `https://${process.env.REACT_APP_DOMAIN_STAGING}/api/v2`;
   envApiUrl3 = `https://${process.env.REACT_APP_DOMAIN_STAGING}/api/v3`;
-  envApiDao = `http://${process.env.REACT_APP_NEST_SERVER_STAG}/api/dao`;
+  envApiDao = `https://${process.env.REACT_APP_NEST_SERVER_STAGING}/api/dao`;
 } else if (env === 'development') {
   envApiUrl = `https://${process.env.REACT_APP_DOMAIN_DEV}/api/v2`;
   envApiUrl3 = `https://${process.env.REACT_APP_DOMAIN_DEV}/api/v3`;
   envApiDao = `http://${process.env.REACT_APP_NEST_SERVER_DEV}/api/dao`;
 } else {
   envApiUrl = `https://${process.env.REACT_APP_DOMAIN_LOCAL}/api/v2`;
-  envApiUrl3 = `http://${process.env.REACT_APP_DOMAIN_LOCAL}/api/v3`;
+  envApiUrl3 = `https://${process.env.REACT_APP_DOMAIN_LOCAL}/api/v3`;
 }
 
 const apiUrl = envApiUrl;
 const apiUrl3 = envApiUrl3;
 const apiDao = envApiDao;
+console.log(env)
+console.log(apiUrl)
+console.log(apiUrl3)
+console.log(apiDao)
 export { apiUrl, apiUrl3, apiDao };
