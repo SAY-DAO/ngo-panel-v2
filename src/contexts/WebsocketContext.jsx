402710e258948c11e8132987d3569ff3e2ea4c6f
Ehsan <ehsan@say.company>
2023-03-02 16:16:59 -0500

    ticketing initiated

--- src/contexts/WebsocketContext.jsx
+++ src/contexts/WebsocketContext.jsx
@@ -1,7 +1,7 @@
 import { createContext } from 'react';
 import { io } from 'socket.io-client';
 
-export const socketHttp = io('http://localhost:8002');
+export const socketHttp = io('https://nest.saydao.org');
 export const WebsocketContext = createContext(socketHttp)
 
 export const WebsocketProvider = WebsocketContext.Provider
