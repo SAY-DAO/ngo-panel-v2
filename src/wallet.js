9a72ecfb6c3bb3c1e459e2e123be002086c8843f
Ehsan <ehsan@say.company>
2023-05-20 18:33:37 +0200

    ticket announcement bug fix

--- src/wallet.js
+++ src/wallet.js
@@ -1,8 +1,9 @@
 import { createClient, configureChains, mainnet } from 'wagmi';
 import { alchemyProvider } from 'wagmi/providers/alchemy';
 import { publicProvider } from 'wagmi/providers/public';
-import { MetaMaskConnector } from 'wagmi/connectors/metaMask';
-import { WalletConnectConnector } from 'wagmi/connectors/walletConnect';
+// import { MetaMaskConnector } from 'wagmi/connectors/metaMask';
+// import { WalletConnectConnector } from 'wagmi/connectors/walletConnect';
+import { createPublicClient, http } from 'viem'
 
 // Two popular providers are Alchemy (alchemy.com) and Infura (infura.io)
 export const { chains, provider, webSocketProvider } = configureChains(
@@ -13,21 +14,25 @@ export const { chains, provider, webSocketProvider } = configureChains(
 // Set up client
 export const wagmiClient = createClient({
   autoConnect: true,
-  connectors: [
-    new MetaMaskConnector({ chains }),
-    // new CoinbaseWalletConnector({
-    //   chains,
-    //   options: {
-    //     appName: 'wagmi',
-    //   },
-    // }),
-    new WalletConnectConnector({
-      chains,
-      options: {
-        projectId: process.env.REACT_APP_WC_PROJECT_ID,
-      },
-    }),
-  ],
+  publicClient: createPublicClient({
+    chain: mainnet,
+    transport: http(),
+  }),
+  // connectors: [
+  //   new MetaMaskConnector({ chains }),
+  //   // new CoinbaseWalletConnector({
+  //   //   chains,
+  //   //   options: {
+  //   //     appName: 'wagmi',
+  //   //   },
+  //   // }),
+  //   new WalletConnectConnector({
+  //     chains,
+  //     options: {
+  //       projectId: process.env.REACT_APP_WC_PROJECT_ID,
+  //     },
+  //   }),
+  // ],
   provider,
   webSocketProvider,
 });
