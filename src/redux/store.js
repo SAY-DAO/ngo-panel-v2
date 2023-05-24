f59af5a10734ad63bbccba374d469cb69033a5a2
Ehsan <ehsan@say.company>
2023-05-24 11:39:30 +0200

    alert fix

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,6 +3,8 @@ import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import reducers from './reducers';
 
+const env = process.env.REACT_APP_NODE_ENV;
+
 const userInfo = localStorage.getItem('userInfo')
   ? JSON.parse(localStorage.getItem('userInfo'))
   : null;
@@ -13,6 +15,10 @@ const initialState = {
     success: userInfo && true,
   },
 };
-const store = createStore(reducers, initialState, composeWithDevTools(applyMiddleware(thunk)));
+const store = createStore(
+  reducers,
+  initialState,
+  env === 'development' && composeWithDevTools(applyMiddleware(thunk)),
+);
 
 export default store;
