e15ca92ac6f6f7f4ceb40375752ee8191486e5b4
Ehsan <ehsan@say.company>
2023-06-22 09:11:45 +0330

    Khordad alert removed

--- src/utils/configs.js
+++ src/utils/configs.js
@@ -1,4 +1,4 @@
 export const NOTIFICATION_TIMER = 2000;
 export const TIMER = 2000;
-export const UNCONFIRMED_NEEDS_THRESHOLD = 120;
+export const UNCONFIRMED_NEEDS_THRESHOLD = 80;
 export const PRODUCT_UNPAYABLE_PERIOD = 14
