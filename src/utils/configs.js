39f24ff8558fa78bc93473e055747b15c5b1e0a5
Gbuntu <ehsan@say.company>
2023-09-03 00:35:18 +0330

    increased the threshhold

--- src/utils/configs.js
+++ src/utils/configs.js
@@ -1,5 +1,5 @@
 export const NOTIFICATION_TIMER = 2000;
 export const TIMER = 2000;
-export const UNCONFIRMED_NEEDS_THRESHOLD = 80;
+export const UNCONFIRMED_NEEDS_THRESHOLD = 200;
 export const PRODUCT_UNPAYABLE_PERIOD = 14
 
