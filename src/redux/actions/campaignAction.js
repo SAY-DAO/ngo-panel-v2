c86b60b6807a67c8e0e646a5641d07bd335e4d95
G <ehsan@say.company>
2024-01-31 17:03:18 +0330

    campaign receivers

--- src/redux/actions/campaignAction.js
+++ src/redux/actions/campaignAction.js
@@ -54,7 +54,7 @@ export const forceSendCampaign = () => async (dispatch, getState) => {
     };
 
     const { data } = await daoApi.get(
-      `/campaign/send`,
+      `/campaign/send/force`,
       config,
     );
 
