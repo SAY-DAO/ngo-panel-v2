182e62e0661ea94533a0997fa6330571194df7d0
G <ehsan@say.company>
2025-11-24 12:44:16 +0330

    small changes + warning

--- public/service-worker.js
+++ public/service-worker.js
@@ -8,8 +8,8 @@
    ================================================================ */
 
 // service-worker.js
-const STATIC_CACHE = 'SAY-PANEL-static-v2.3.5'; // bump this version when you deploy
-const RUNTIME_CACHE = 'SAY-PANEL-runtime-v2.3.5';
+const STATIC_CACHE = 'SAY-PANEL-static-v2.3.6'; // bump this version when you deploy
+const RUNTIME_CACHE = 'SAY-PANEL-runtime-v2.3.6';
 
 const PRECACHE_URLS = [
   '/',
