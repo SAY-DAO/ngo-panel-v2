447782a6476caee412b07aa448fff8866b87b2be
G <ehsan@say.company>
2026-02-16 10:04:12 +0330

    metamsk error

--- src/wallet.js
+++ src/wallet.js
@@ -1,13 +1,13 @@
 /* eslint-disable import/prefer-default-export */
 import { http, createConfig } from 'wagmi';
 import { mainnet, base } from 'wagmi/chains';
-import { metaMask } from 'wagmi/connectors';
+// import { metaMask } from 'wagmi/connectors';
 
 export const config = createConfig({
   chains: [mainnet, base],
   connectors: [
     // walletConnect({ projectId: process.env.REACT_APP_WC_PROJECT_ID }),
-    metaMask(),
+    // metaMask(),
   ],
   transports: {
     [mainnet.id]: http(),
